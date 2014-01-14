b6c6a9504928896fa7f53ecbf852a4ecfb1a1d33
Hao Hou <ghost89413@gmail.com>
2014-01-13 18:36:59 -0700

    ...

--- dalvik/dalvik_ins.c
+++ dalvik/dalvik_ins.c
@@ -11,44 +11,44 @@ size_t _dalvik_instruction_pool_size = 0;
 
 int _dalvik_instruction_pool_resize()
 {
-	dalvik_instruction_t* old_pool;
-	assert(dalvik_instruction_pool != NULL);
-	
-	old_pool = dalvik_instruction_pool;
-	
-	dalvik_instruction_pool = realloc(old_pool, _dalvik_instruction_pool_capacity * 2);
+    dalvik_instruction_t* old_pool;
+    assert(dalvik_instruction_pool != NULL);
+    
+    old_pool = dalvik_instruction_pool;
+    
+    dalvik_instruction_pool = realloc(old_pool, _dalvik_instruction_pool_capacity * 2);
 
-	if(NULL == dalvik_instruction_pool) 
-	{
-		dalvik_instruction_pool = old_pool;
-		return -1;
-	}
-	_dalvik_instruction_pool_capacity *= 2;
-	return 0;
+    if(NULL == dalvik_instruction_pool) 
+    {
+        dalvik_instruction_pool = old_pool;
+        return -1;
+    }
+    _dalvik_instruction_pool_capacity *= 2;
+    return 0;
 }
 
 int dalvik_instruction_init( void )
 {
-	_dalvik_instruction_pool_size = 0;
-	if(NULL == dalvik_instruction_pool) 
-		dalvik_instruction_pool = (dalvik_instruction_t*)malloc(sizeof(dalvik_instruction_t) * _dalvik_instruction_pool_capacity);
-	if(NULL == dalvik_instruction_pool)
-		return -1;
-	return 0;
+    _dalvik_instruction_pool_size = 0;
+    if(NULL == dalvik_instruction_pool) 
+        dalvik_instruction_pool = (dalvik_instruction_t*)malloc(sizeof(dalvik_instruction_t) * _dalvik_instruction_pool_capacity);
+    if(NULL == dalvik_instruction_pool)
+        return -1;
+    return 0;
 }
 
 int dalvik_instruction_finalize( void )
 {
-	if(dalvik_instruction_pool != NULL) free(dalvik_instruction_pool);
-	return 0;
+    if(dalvik_instruction_pool != NULL) free(dalvik_instruction_pool);
+    return 0;
 }
 
 dalvik_instruction_t* dalvik_instruction_new( void )
 {
-	if(_dalvik_instruction_pool_size >= _dalvik_instruction_pool_capacity)
-	{
-		if(_dalvik_instruction_pool_resize() < 0) return NULL;
-	}
-	return dalvik_instruction_pool + (_dalvik_instruction_pool_size ++);
+    if(_dalvik_instruction_pool_size >= _dalvik_instruction_pool_capacity)
+    {
+        if(_dalvik_instruction_pool_resize() < 0) return NULL;
+    }
+    return dalvik_instruction_pool + (_dalvik_instruction_pool_size ++);
 }
 
