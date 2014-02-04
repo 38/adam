e12d27b19f35468882e689d6b3205ed2516f9135
ghost <ghost@ghost-HP-Compaq-8000-Elite-CMT-PC.(none)>
2014-02-04 15:25:01 -0700

    rollback the bad commit and start new cesk machine

---
+++ include/cesk/cesk_value.h
@@ -0,0 +1,51 @@
+#ifndef __CESK_VALUE_T__
+#define __CESK_VALUE_T__
+#include <constants.h>
+
+/* previous defination */
+struct   _cesk_value_t;
+typedef  struct _cesk_value_t cesk_value_t;
+struct   _cesk_value_set_t;
+typedef  struct _cesk_value_set_t cesk_value_set_t;
+
+#include <dalvik/dalvik_instruction.h>
+
+/* Abstract Value */
+enum{
+    CESK_TYPE_NUMERIC,   /* we treat char as a numeric value */
+    CESK_TYPE_STRING,
+    CESK_TYPE_BOOLEAN,
+    CESK_TYPE_OBJECT,
+    CESK_TYPE_ARRAY
+};
+
+/* abstract numeric value */
+#define CESK_VALUE_NUMBERIC_POSITIVE 1  /* if a numeric value is positive */
+#define CESK_VALUE_NUMERIC_ZERO 0       /* if a numeric value is zero */
+#define CESK_VALUE_NUMBERIC_NEGATIVE -1 /* if a numeric value is negative */
+typedef int8_t cesk_value_numeric_t;    /* a numberic value */
+
+/* abstract string value */
+/* Nothing to abstract, just the type itself */
+
+/* abstract boolean */
+#define CESK_VALUE_BOOLEAN_TRUE  1
+#define CESK_VALUE_BOOLEAN_FALSE 0
+typedef int8_t cesk_value_boolean_t;
+
+/* abstract object */
+#include <cesk/cesk_object.h>
+
+/* abstract array */
+typedef struct {
+    cesk_value_set_t* values;
+} cesk_value_array_t;
+
+typedef struct _cesk_value_t {
+} cesk_value_t;   /* this is a abstract value */
+
+
+typedef struct _cesk_value_set_t {
+    
+} cesk_value_set_t;
+#endif /* __CESK_VALUE_T__ */
