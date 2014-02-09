ad7a28bdb1bf109ea37630b65b0382c1bc6c8aab
ghost <ghost@ghost-HP-Compaq-8000-Elite-CMT-PC.(none)>
2014-02-08 17:24:35 -0700

    TODO: set join and push

--- include/cesk/cesk_set.h
+++ include/cesk/cesk_set.h
@@ -21,7 +21,7 @@ int cesk_set_join(cesk_set_t* dest, cesk_set_t* sour);
 int cesk_set_push(cesk_set_t* dest, uint32_t addr);
 
 /* return a iterator that used for tranverse the set */
-cesk_set_iter_t* cesk_set_iter(cesk_set_t* set);
+cesk_set_iter_t* cesk_set_iter(cesk_set_t* set, cesk_set_iter_t* buf);
 
 /* advance the iterator, the value is the next address in the set
  * If the function returns CESK_STORE_ADDR_NULL, that means there's
@@ -33,7 +33,12 @@ uint32_t cesk_set_iter_next(cesk_set_iter_t* iter);
 size_t cesk_set_size(cesk_set_t* set);
 
 /* fork a set */
-int cesk_set_fork(cesk_set_t* sour);
+cesk_set_t* cesk_set_fork(cesk_set_t* sour);
 /* despose a set */
 void cesk_set_free(cesk_set_t* set);
+
+/* init & finalize */
+void cesk_set_init();
+void cesk_set_finalize();
+
 #endif /* __CESK_SET_H__ */
