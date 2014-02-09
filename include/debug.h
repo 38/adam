fa5ff1c257b82837dc7dbbc7446745d0f9c2da20
ghost <ghost@ghost-HP-Compaq-8000-Elite-CMT-PC.(none)>
2014-02-08 21:32:46 -0700

    comments

--- include/debug.h
+++ include/debug.h
@@ -1,6 +1,10 @@
 #ifndef __DEBUG_H__
+/* 
+ * this file contains some debug code.
+ */
 #include <log.h>
 #ifdef PRINT_FREE
+/* print a log for memory free */
 #   define free(mem) do{ LOG_TRACE("free memory@0x%x", mem); free(mem);}while(0)
 #endif
 #endif
--- include/log.h
+++ include/log.h
@@ -1,5 +1,29 @@
 #ifndef __LOG_H__
 #define __LOG_H__
+/*
+ * log.h: Macros for logging. 
+ * You can use LOG_<LOG_LEVEL> to output a log in the code.
+ * There are 6 log levels, you can find them in the enumerate below
+ * e.g.:
+ *       p = malloc(1024);
+ *       if(NULL == p) LOG_ERROR("something was wrong!");
+ * 
+ * By default, all log will be printed to stderr. But you can write a 
+ * log.conf in the configure directory (defined in constants.h).
+ * 
+ * Sample configure file :
+ *      # This is a comment
+ *      # level    file name      mode
+ *        DEBUG   /tmp/debug.log    a
+ *        WARNING /dev/null             # WARNING will be ignored
+ *
+ *        default <stderr>              # other level will be printed to stderr
+ *
+ *
+ * THIS FILE NEEDS INITIALIZATION BEFORE YOU CAN USE THE FUNCTION
+ */
+
+/* log levels */
 enum{
     FATAL,
     ERROR,
@@ -9,13 +33,19 @@ enum{
     TRACE,
     DEBUG
 };
+
+/* Initialization & Finalization */
 void log_init();
 void log_finalize();
+
+/* the implementation of write a log */
 void log_write(int level, const char* file, const char* function, int line, const char* fmt, ...);
 
+/* helper macros for write a log, do not use it directly */
 #define __LOG__(level,fmt,arg...) do{\
         log_write(level,__FILE__,__FUNCTION__,__LINE__,fmt, ##arg);\
 }while(0)
+
 #ifndef LOG_LEVEL
         #define LOG_LEVEL 6
 #endif
