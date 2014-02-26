9a9c1dffec1bec63e5f224583b7dff4ebdcc0666
Hao Hou <ghost89413@gmail.com>
2014-02-26 16:30:52 -0700

    finish implementing reloc

--- src/cesk/cesk_reloc.c
+++ src/cesk/cesk_reloc.c
@@ -4,6 +4,8 @@
  * @file cesk_reloc.c
  * @brief implementation of relocation table
  * @todo  how to reduce the memory allocation cost
+ * @todo  if the conflict is really rear, try to use a bloom filter to optimize this
+ * @todo test it
  */
 /**
  * @brief the relocate table node 
@@ -15,7 +17,72 @@ typedef struct _cesk_reloc_table_node_t {
 } cesk_reloc_table_node_t;
 struct _cesk_reloc_table_t {
 	cesk_reloc_table_node_t* htab[CESK_RELOC_TABLE_SIZE];
-}; 
+};
+/**
+ * @brief the hash code for the relocate table 
+ **/
+static inline hashval_t _cesk_reloc_table_entry_hashcode(uint32_t fromaddr)
+{
+	return fromaddr;
+}
+/** 
+ * @brief insert an entry to the relocation table
+ * @detials here we assume the table pointer is always valid
+ * @return result of insertion operate
+ **/
+static inline int _cesk_reloc_table_insert(cesk_reloc_table_t* table, uint32_t fromaddr, uint32_t toaddr)
+{
+	hashval_t h = _cesk_store_address_hashcode(fromaddr) % CESK_RELOC_TABLE_SIZE;
+	cesk_reloc_table_node_t* ptr;
+	for(ptr = table->htab[h]; NULL != ptr; ptr = ptr->next)
+	{
+		if(ptr->from == fromaddr)
+		{
+			LOG_DEBUG("find duplicated relocate table entry");
+			break;
+		}
+	}
+	/* if we find some entry that is already there */
+	if(NULL != ptr)
+	{
+		if(ptr->toaddr != toaddr)
+		{
+			LOG_WARNING("ooops, there's different entries in the same key. does anything goes wrong?");
+			return -1;
+		}
+		LOG_DEBUG("there's an entry already there");
+		return 0;
+	}
+	/* if we didn't find anything, then create a new node for this */
+	cesk_reloc_table_t* newnode = (cesk_reloc_table_node_t*)malloc(sizeof(cesk_reloc_table_node_t));
+	if(NULL == newnode)
+	{
+		LOG_ERROR("can not allocate memory for relocate table");
+		return -1;
+	}
+	newnode->from = fromaddr;
+	newnode->to = toaddr;
+	newnode->next = table->htab[h];
+	table->htab[h] = newnode;
+	LOG_DEBUG("relocating address @0x%x to @0x%x", fromaddr, toaddr);
+	return 0;
+}
+void cesk_reloc_table_free(cesk_reloc_table_t* table)
+{
+	if(NULL == table) return;
+	int i;
+	for(i = 0; i < CESK_RELOC_TABLE_SIZE; i ++)
+	{
+		cesk_reloc_table_node_t* ptr;
+		for(ptr = table->htab[i]; NULL == ptr;)
+		{
+			cesk_reloc_table_node_t* old = ptr;
+			ptr = ptr->next;
+			free(ptr);
+		}
+	}
+	free(table);
+}
 cesk_reloc_table_t* cesk_reloc_table_from_store(cesk_store_t** p_dest, const cesk_store_t* sour)
 {
 	cesk_reloc_table_t*  ret = NULL;
@@ -27,6 +94,12 @@ cesk_reloc_table_t* cesk_reloc_table_from_store(cesk_store_t** p_dest, const ces
 	cesk_store_t* dest = *p_dest;
 
 	ret = (cesk_reloc_table_t*)malloc(sizeof(cesk_reloc_table_t));
+	if(NULL == ret)
+	{
+		LOG_WARNING("can not allocate memory for the relocation table");
+		goto ERROR;
+	}
+	memset(ret, 0, sizeof(cesk_reloc_table_t));
 	/* because we do not delete any block in the block list, so the first part in each store should 
 	 * definately be couterparts
 	 */
@@ -76,16 +149,40 @@ cesk_reloc_table_t* cesk_reloc_table_from_store(cesk_store_t** p_dest, const ces
 				LOG_WARNING("can not allocate new address for the conflict object");
 				continue;
 			}
-			LOG_DEBUG("find relocation plan: @0x%x --> @0x%x", i * CESK_STORE_BLOCK_NSLOTS + j, new_addr);
-			//TODO: add to the allocation table
+			
+			/* because the p_dest might change, so we must update the value of dest now */
+			dest = *p_dest;
+
+			/* okay, build an actually relocation rule */
+			uint32_t old_addr = i * CESK_STORE_BLOCK_NSLOTS + j;
+			LOG_DEBUG("find relocation plan: @0x%x --> @0x%x", old_addr, new_addr);
+			/* now we insert the entry to the table */
+			if(_cesk_reloc_table_insert(ret, old_addr, new_addr) < 0)
+			{
+				LOG_WARNING("can not build relocation rule from @0x%x to @0x%x", old_addr, new_addr);
+				continue;
+			}
 		}
 	}
+	LOG_DEBUG("relocate table has been built for store@%p to store@%p", *p_dest, sour);
+
+	return 0;
 ERROR:
 	LOG_ERROR("can not build object relocation table");
-	if(NULL != ret)
-	{
-		free(ret);
-	}
+	cesk_reloc_free(ret);
 	return -1;
 }
 
+uint32_t cesk_reloc_table_look_for(const cesk_reloc_table_t* table, uint32_t addr)
+{
+	if(NULL == table || CESK_STORE_ADDR_IS_CONST(addr))
+	{
+		LOG_ERROR("invalid argument");
+		return CESK_STORE_ADDR_NULL;
+	}
+	int h = _cesk_store_address_hashcode(addr);
+	cesk_reloc_table_node_t* ptr;
+	for(ptr = table->htab[h]; NULL != ptr; ptr = ptr->next)
+		if(ptr->from == addr) return ptr->to;
+	return NULL;
+}
