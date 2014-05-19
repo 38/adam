deb1e96e3c13d45c66df86cc72aa60d861a63e2a
Hao Hou <ghost89413@gmail.com>
2014-05-19 13:55:55 -0600

    sync

--- src/cesk/cesk_diff.c
+++ src/cesk/cesk_diff.c
@@ -3,9 +3,20 @@
  * @brief implementation of frame diff package
  * @todo review the code, and the idea of diff
  **/
-
+#include <const_assertion.h>
 #include <cesk/cesk_diff.h>
 #include <vector.h>
+/**
+ * @brief define the segmentations in a diff pacakge 
+ **/
+enum{
+	CESK_DIFF_ALLOC,
+	CESK_DIFF_REUSE,
+	CESK_DIFF_REG,
+	CESK_DIFF_STORE,
+	CESK_DIFF_DEALLOC,
+	CESK_DIFF_NTYPES
+};
 typedef struct _cesk_diff_segment_node_t _cesk_diff_segment_node_t;
 typedef struct _cesk_diff_segment_t _cesk_diff_segment_t;
 /**
@@ -19,3 +30,12 @@ struct _cesk_diff_segment_node_t{
 		uint32_t      uint32;      /*!< this is a uint32_t type */  
 	} value;    /*!< the value to set, if it's in the reuse section, value means the new reuse flag */
 };
+/**
+ * @brief the diff package
+ **/
+struct _cesk_diff_segment_t{
+	int size[CESK_DIFF_NTYPES];   /*!< the size of each segment */
+	_cesk_diff_segment_node_t data[0]; /*!< the data section */
+};
+CONST_ASSERTION_LAST(_cesk_diff_segment_t, data);
+CONST_ASSERTION_SIZE(_cesk_diff_segment_t, data, 0);
