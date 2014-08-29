53ca85b1008c5453f410ecf87a35ecfe31095978
Hao Hou <ghost89413@gmail.com>
2014-08-29 13:29:26 -0600

    syn

---
+++ include/trac/trac.h
@@ -0,0 +1,18 @@
+/**
+ * @file trac.h
+ * @brief The top level header file for tag tracker
+ **/
+#ifndef __TRAC_H__
+/**
+ * @brief initialization
+ * @return < 0 if failed to initialize
+ **/
+int trac_init();
+/**
+ * @brief finalization
+ * @return nothing
+ **/
+void trac_finalization();
+
+#endif
+
---
+++ src/trac/trac.c
@@ -0,0 +1,9 @@
+#include <trac.h>
+int trac_init()
+{
+	return 0;
+}
+void trac_finalize()
+{
+	return 0;
+}
